2c7f9faf63cf5bf57ac3231e7ff5731f154b9561
Lucas Pinheiro <lucaspinheiroalberdanha@gmail.com>
2022-08-26 15:37:37 -0300

    feat and chore: react-ts project generated by vite, with react router

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import {
+  Header,
+  Footer,
+} from '../../components'
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  )
+}
+
+export default Home;
---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Home from './Home'
+import Edit from './Edit'
+import Delete from './Delete'
+import Create from './Create'
+
+export {
+  Home,
+  Edit,
+  Delete,
+  Create,
+}
